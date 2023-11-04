67a36cb0b69d330e63859993671bdd0c27fc32ae
Avaz <elonarm21@gmail.com>
2023-11-04 20:09:19 +0500

    About section html completed

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import AboutUs from "../pages/AboutUs";
 
 const App = () => {
     return (
         <div className={"app"}>
-
+            <h1>eLearning</h1>
+            <AboutUs/>
         </div>
     );
 };
