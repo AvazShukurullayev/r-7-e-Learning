c6c5b37952c26b26e4295e8e792cc89aa14699b1
Avaz <elonarm21@gmail.com>
2023-11-04 23:26:40 +0500

    Benefit section completed

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import homeIntro from "../img/home_intro.jpg"
-import styled from "styled-components";
+import {About, Description, Image, Hide} from "../styles/styles";
 
 const AboutSection = () => {
     return (
@@ -29,29 +29,4 @@ const AboutSection = () => {
     );
 };
 
-// Styled Components
-const About = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 2rem;
-`
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-`
-const Hide = styled.div`
-  overflow: hidden;
-`
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-
-  img {
-    width: 100%;
-    height: 90vh;
-    object-fit: cover;
-    object-position: top center;
-  }
-`
 export default AboutSection;
--- src/components/BenefitsSection.js
+++ src/components/BenefitsSection.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import benefitLab from "../img/benefit_lab.jpg"
+import styled from "styled-components";
+import {About, Description, Image} from "../styles/styles";
 
 const BenefitsSection = () => {
     return (
-        <div className={"benefits__section"}>
-            <div className="description">
-                <h2>High quality learning system</h2>
+        <Benefits>
+            <Description>
+                <h2>High-Quality Learning System</h2>
                 <div className="benefit">
                     <h3>Online platform</h3>
                     <h3>High experienced and talented mentors</h3>
                     <h3>Fast learning system</h3>
                     <h3>Offline school for children</h3>
                 </div>
-            </div>
-            <div className="image">
+            </Description>
+            <Image>
                 <img src={benefitLab} alt="woman is working on lab"/>
-            </div>
-        </div>
+            </Image>
+        </Benefits>
     );
 };
+
+const Benefits = styled(About)`
+  flex-direction: row-reverse;
+  h2 {
+    color: #30bee1;
+    padding-bottom: 5rem;
+  }
+  
+  h3 {
+    font-size: 1.4rem;
+    padding: 1rem;
+    background-color: #fff;
+    font-weight: 400;
+    color: #000;
+    margin: 1rem;
+    width: 70%;
+    border-left: 4px solid #30bee1;
+  }
+`
 
 export default BenefitsSection;
